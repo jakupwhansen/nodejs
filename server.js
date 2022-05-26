cb8086b7912b2b00f8b1432bf4eb9711ac4f485a
Jákup Wenningstedt Hansen <jakupwhansen@hotmail.com>
2022-05-26 15:09:43 +0200

    lille test ændring her fra gitHub

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World jakup eksempel 2\n');
+    res.end('Hello World jakup eksempel 2, ændret fra gitHub\n');
 }).listen(port);
